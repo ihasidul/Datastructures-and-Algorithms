f32b89fef33955b8fa5ac424bb6e5f0b086b5392
ihasidul <ihasidul@gmail.com>
2019-03-27 20:36:49 +0600

    GOt the initial idea for SJF

--- Scheduling Algorithms/Shortest_job_first.cpp
+++ Scheduling Algorithms/Shortest_job_first.cpp
@@ -2,9 +2,13 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
+//This should be the non-preemtive one
+//TODO write a class or struct with the Pno,At,BT,
 int main()
 {
-    
+//formula : TAT = CT - AT
+//formula : WT = TAT - BT
+//TODO write function or print them
+//another aproce is to add this TAT and the  WT as a attribute
     return 0;
 }
