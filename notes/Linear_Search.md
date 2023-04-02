997e58c8c1386f0fb9cdcb6367cc3159300996e0
ihasidul <ihasidul@gmail.com>
2023-04-03 00:23:49 +0600

    Add Linear Search

---
+++ notes/Linear_Search.md
@@ -0,0 +1,5 @@
+# Linear Search
+
+The linear search algorithm is a very simple algorithm that searches for a value in an array by checking each element in the array until it finds the value or reaches the end of the array.
+
+- O(n) time complexity
--- searching/linear_search.py
+++ searching/linear_search.py
@@ -1,12 +1,12 @@
 def linear_search(arr,search_item):
     """
-    Linear search algorithm
+    Linear search algorithm. Return True if search_item is in arr, else False.
     """
     for i in range(len(arr)):
         if arr[i] == search_item:
-            return i
-    return -1
+            return True
+    return False
 
 arr = [1,2,3,10,412,234,12,9]
-search_item = 12
+search_item = 10
 print(linear_search(arr,search_item))
